c188cd649835711c7589a95d776150803924070b
MichaelPisciotta <mfp56@miami.edu>
2021-08-11 16:22:31 -0400

    done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let button = document.getElementById('get-quote').addEventListener('click', () => fetchJoke())
+//grabs button from html and adds event listener 
 
 function fetchJoke(){
     fetch('https://api.kanye.rest')
@@ -10,6 +11,8 @@ function fetchJoke(){
     }
     )
 }
+//calls info from kanye tweets API and returns usable JSON data
+//quote is extracted from data and set to variable to be used in displayQuote 
 
 function displayQuote(postedQuote){
     const p = document.createElement('p')
@@ -21,26 +24,37 @@ function displayQuote(postedQuote){
     document.getElementById('quote-print-div').appendChild(p)
 
 }
+//creates a p tag and sets its value to whatever postedQuote is
+//then creates button and adds eventlistener: when click run function addToSaved
+//the button is then given the value of '+' and appended to the paragraph tag 
+//finally I grab the div where i want quotes to print into and append the p tag which nows also has the button
 
 function addToSaved(e){
-document.getElementById('save-quote-list').appendChild(e.target.parentNode) //e=click, tgt='+'btn, parent=quote
-// const deleteButton = document.createElement('button') 
-// deleteButton.textContent = '-'
-// e.target.parentNode.appendChild(deleteButton)
-// deleteButton.addEventListener('click', deleteButton)
+document.getElementById('save-quote-list').appendChild(e.target.parentNode) //grab savequotes div and append quote being passed in -- e=click, tgt='+'btn, parent=quote
+const deleteButton = document.createElement('button') 
+deleteButton.textContent = '-'
+debugger
+e.target.parentNode.appendChild(deleteButton) //trying to add the - button
+deleteButton.addEventListener('click', deleteButton) //trying to add EL to - button
 }
 
+function deleteButton(e){
+    debugger
+e.target.parentNode.remove() 
+}
+//tried creating function that, when called, would remove the entire quote line
 
-// function deleteButton(e){
-// e.target.parentNode.remove(e)
-// }
+//PROBLEM: 
+//1. cant figure out how to replace + with a - and create a button to delete items from saved list
+//2. cant figure out how to make quotes print one at a time, the new one replacing the old
 
 
 
 
-//figure out how to make it so only one quote shows at a time 
-//add button to printed items that allows you to click and add them to favorite list 
-//add button to items in favorite list to remove from the list 
+
+
+
+
 
 
 
